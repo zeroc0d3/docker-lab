849c6e5f489f25fc9709b5fe44d50c832a5dc604
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2017-10-03 13:53:34 +0700

    Update running script environments for new container mongodb

--- env_development.sh
+++ env_development.sh
@@ -1,11 +1,9 @@
-
 #!/bin/sh
 
 docker-compose build
 
 ## Just Running Some Containers ##
-# docker-compose up consul nginx ruby postgresql
+# docker-compose up consul nginx ruby postgresql mongodb
 
 ## Recreate Container ##
-docker-compose up --force-recreate consul nginx ruby postgresql
-
+docker-compose up --force-recreate consul nginx ruby postgresql mongodb
--- env_production.sh
+++ env_production.sh
@@ -3,7 +3,7 @@
 docker-compose build
 
 ## Just Running Some Containers ##
-# docker-compose up consul nginx ruby postgresql mariadb redis
+# docker-compose up consul nginx ruby postgresql mariadb mongodb redis
 
 ## Recreate Container ##
-docker-compose up --force-recreate consul nginx ruby postgresql mariadb redis
+docker-compose up --force-recreate consul nginx ruby postgresql mariadb mongodb redis
